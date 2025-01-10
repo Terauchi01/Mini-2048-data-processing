419dd08eff262508026df71550eaade10ec6c49c
250304s <250304s@ugs.kochi-tech.ac.jp>
2025-01-10 15:38:06 +0900

    得点分布のグラフを仮実装(graphからは実行不可)

---
+++ graph/histgram.py
@@ -0,0 +1,37 @@
+import re
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+BINS = 100
+
+
+def plot_histgram(
+    state_files: list[Path],
+    output: Path,
+    config: dict = {},
+):
+    """
+    得点分布をプロットする。
+    """
+    for i, state_file in enumerate(state_files):
+        text = state_file.read_text()
+        score_txt = re.findall(r"score: (\d+)", text)
+        scores = [int(score) for score in score_txt]
+
+        plt.hist(scores, bins=BINS, label=state_file.stem)
+        plt.xlabel("score")
+        plt.ylabel("frequency")
+        plt.legend()
+        plt.savefig(output.with_stem(f"{output.stem}-{i}"))
+
+
+if __name__ == "__main__":
+    plot_histgram(
+        state_files=[
+            Path("board_data/CNN_DEEP/state.txt"),
+            # Path("NN-DEEP/state.txt"),
+            # Path("NT4/state.txt"),
+        ],
+        output=Path("histgram.png"),
+    )
