1d3a79ae1ec5ba6dc89abfc2ba35294370f5d044
250304s <250304s@ugs.kochi-tech.ac.jp>
2025-01-10 15:44:05 +0900

    histgramの微修正

--- graph/histgram.py
+++ graph/histgram.py
@@ -19,19 +19,20 @@ def plot_histgram(
         score_txt = re.findall(r"score: (\d+)", text)
         scores = [int(score) for score in score_txt]
 
-        plt.hist(scores, bins=BINS, label=state_file.stem)
+        plt.hist(scores, bins=BINS)
         plt.xlabel("score")
         plt.ylabel("frequency")
-        plt.legend()
         plt.savefig(output.with_stem(f"{output.stem}-{i}"))
+        plt.close()
 
 
 if __name__ == "__main__":
     plot_histgram(
         state_files=[
             Path("board_data/CNN_DEEP/state.txt"),
+            Path("board_data/PP/state.txt"),
             # Path("NN-DEEP/state.txt"),
             # Path("NT4/state.txt"),
         ],
-        output=Path("histgram.png"),
+        output=Path("./output/histgram.pdf"),
     )
